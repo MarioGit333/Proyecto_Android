b5a96f9ce791d5635464859e2b694484cb1ebe9f
beatboxgascon <32103615+beatboxgascon@users.noreply.github.com>
2017-12-01 16:39:01 +0100

    Funcionalidad NavDrawer

--- app/src/main/java/com/alexmario/proyecto/proyecto_android/PantallaPrincipal.java
+++ app/src/main/java/com/alexmario/proyecto/proyecto_android/PantallaPrincipal.java
@@ -1,9 +1,7 @@
 package com.alexmario.proyecto.proyecto_android;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -22,15 +20,6 @@ public class PantallaPrincipal extends AppCompatActivity
         setContentView(R.layout.activity_pantalla_principal);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -80,15 +69,13 @@ public class PantallaPrincipal extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
+        if (id == R.id.pantalla1) {
+            startActivity(new Intent(this,MainActivity.class));
+        } else if (id == R.id.pantalla2) {
+            startActivity(new Intent(this,Main2Activity.class));
+        } else if (id == R.id.pantalla3) {
+            startActivity(new Intent(this,Main3Activity.class));
+        } else if (id == R.id.pantalla4) {
 
         } else if (id == R.id.nav_send) {
 
