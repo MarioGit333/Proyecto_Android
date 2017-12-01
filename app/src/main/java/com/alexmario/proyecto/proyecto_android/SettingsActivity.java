2b8d3849397173fd74770cf8bc4fedbe7db9e415
MarioGit333 <32578491+MarioGit333@users.noreply.github.com>
2017-12-01 18:29:31 +0100

    Cambio revertido del boton "atras".

--- app/src/main/java/com/alexmario/proyecto/proyecto_android/SettingsActivity.java
+++ app/src/main/java/com/alexmario/proyecto/proyecto_android/SettingsActivity.java
@@ -183,7 +183,7 @@ public class SettingsActivity extends AppCompatPreferenceActivity {
         public boolean onOptionsItemSelected(MenuItem item) {
             int id = item.getItemId();
             if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), PantallaPrincipal.class));
+                startActivity(new Intent(getActivity(), SettingsActivity.class));
                 return true;
             }
             return super.onOptionsItemSelected(item);
