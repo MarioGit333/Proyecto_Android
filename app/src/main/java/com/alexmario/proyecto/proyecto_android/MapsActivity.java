84f7535023bf86a41aa3bfe58d786fd68d3b6ac1
beatboxgascon <32103615+beatboxgascon@users.noreply.github.com>
2017-12-18 18:05:56 +0100

    Actualizado MapsActivity

--- app/src/main/java/com/alexmario/proyecto/proyecto_android/MapsActivity.java
+++ app/src/main/java/com/alexmario/proyecto/proyecto_android/MapsActivity.java
@@ -1,23 +1,42 @@
 package com.alexmario.proyecto.proyecto_android;
 
-import android.support.v4.app.FragmentActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
+    Button botonmapa,botonsatelite,botonhibrido,botonInterior;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        botonmapa=findViewById(R.id.botonmapa);
+        botonsatelite=findViewById(R.id.botonsatelite);
+        botonhibrido=findViewById(R.id.botonhibrido);
+        botonInterior=findViewById(R.id.botonInterior);
+
+        botonmapa.setOnClickListener(this);
+        botonsatelite.setOnClickListener(this);
+        botonhibrido.setOnClickListener(this);
+        botonInterior.setOnClickListener(this);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
@@ -37,10 +56,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        final PolylineOptions rectOptions = new PolylineOptions();
+
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
+            @Override
+            public void onMapLongClick(LatLng latLng) {
+
+                rectOptions.add(latLng).width(10)
+                        .color(Color.BLUE)
+                        .geodesic(true);
+                Polyline polyline= mMap.addPolyline(rectOptions);
+            }
+        });
+
+        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+            @Override
+            public boolean onMarkerClick(Marker marker) {
+                Toast.makeText(getApplicationContext(),"Me pulsiaste jaj :V",Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.botonmapa:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case R.id.botonsatelite:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case R.id.botonhibrido:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+            case R.id.botonInterior:
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(-33.86997,151.2089),18));
+                break;
+        }
     }
 }
